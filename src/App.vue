3d0ff255c08fa8a15c60389f80aea37f3d7e1494
Saad-git <m.saadshaukat@gmail.com>
2020-10-30 12:02:47 +0500

    adds app to git

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Saad's first Vue.js App"/>
   </div>
 </template>
 
